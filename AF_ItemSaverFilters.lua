a8fb4c827a7f87d422b4a102c09b82590496d272
Randactyl <rafowler@randactyl.com>
2015-08-30 22:07:30 -0700

    update addon and API version

--- AF_ItemSaverFilters.lua
+++ AF_ItemSaverFilters.lua
@@ -1,10 +1,10 @@
 --[[
-    This function handles the actual filtering. Use whatever parameters for "GetFilterCallback..." 
+    This function handles the actual filtering. Use whatever parameters for "GetFilterCallback..."
     and whatever logic you need to in "function( slot )". A return value of true means the item in
     question will be shown while the filter is active.
   ]]
 local function GetFilterCallbackForSaved()
-	return function( slot )
+	return function(slot)
 		return ItemSaver_IsItemSaved(slot.bagId, slot.slotIndex)
 	end
 end
@@ -14,11 +14,11 @@ end
     callback table. The string value for name is the relevant key for the language table.
   ]]
 local ItemSaverDropdownCallback = {
-	[1] = { name = "Saved", filterCallback = GetFilterCallbackForSaved()}
+	[1] = { name = "Saved", filterCallback = GetFilterCallbackForSaved() }
 }
 
 --[[
-    There are four potential tables for this section each covering either english, german, french, 
+    There are four potential tables for this section each covering either english, german, french,
     or russian. Only english is required. If other language tables are not included, the english
     table will automatically be used for those languages. All languages must share common keys.
   ]]
@@ -28,12 +28,12 @@ local strings = {
 
 --[[
     This section packages the data for Advanced Filters to use.
-    All keys are required except for deStrings, frStrings, and ruStrings, as they correspond to 
+    All keys are required except for deStrings, frStrings, and ruStrings, as they correspond to
         optional languages. Al language keys are assigned the same table here only to demonstrate
         the key names. You do not need to do this.
     The filterType key expects an ITEMFILTERTYPE constant provided by the game.
     The values for key/value pairs in subfilters can be any of the string keys from lines 127 - 218
-        of AdvancedFiltersData.lua (AF_Callbacks table) such as "All", "OneHanded", "Body", or 
+        of AdvancedFiltersData.lua (AF_Callbacks table) such as "All", "OneHanded", "Body", or
         "Blacksmithing".
     If your filterType is ITEMFILTERTYPE_ALL then subfilters must only contain the value "All".
   ]]
